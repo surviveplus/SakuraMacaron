50d4d24aed93173793bc5749ff319ead9aae2c5f
Shin-ichi Koga <shin-ichi@surviveplus.net>
2016-02-09 21:59:20 +0900

    create ExcelMacaron. implement ReplaceSelectionText.

---
+++ SakuraMacaron.OfficeAddIn.Excel/SakuraMacaron.OfficeAddIn.Excel/ExcelMacaron.vb
@@ -0,0 +1,120 @@
+﻿Imports Microsoft.Office.Interop.Excel
+Imports Net.Surviveplus.SakuraMacaron.Core
+
+Public Class ExcelMacaron
+    Inherits Macaron
+
+    ' Overrides or implements
+
+#Region " Macaron members "
+
+    Public Overrides Sub ReplaceSelectionParagraphs(prepare As Action(Of TextActionsParameters), act As Action(Of TextActionsParameters))
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub ReplaceSelectionText(prepare As Action(Of TextActionsParameters), act As Action(Of TextActionsParameters))
+
+        Dim target As Range = Me.app.Selection
+
+        Dim getText =
+            Function(cell As Range) As String
+                Return cell.Formula
+            End Function
+
+        Dim setText =
+            Sub(cell As Range, text As String)
+                cell.Formula = text
+            End Sub
+
+        Dim r = ForEachRange(target, prepare, getText, Nothing)
+        If r.IsCanceld = False Then ForEachRange(target, act, getText, setText)
+
+    End Sub
+
+#End Region
+
+
+    ' Class members
+
+#Region " Constructors "
+
+    ''' <summary>
+    ''' Initializes a new instance of the class.
+    ''' </summary>
+    ''' <param name="app">Set Application object.</param>
+    ''' <remarks></remarks>
+    Public Sub New(app As Application)
+        If app Is Nothing Then Throw New ArgumentNullException("app")
+        Me.app = app
+    End Sub
+
+    Private app As Application
+
+#End Region
+
+    Private Shared Function ForEachRange(
+        target As Range,
+        act As Action(Of TextActionsParameters),
+        getText As Func(Of Range, String),
+        setText As Action(Of Range, String)
+        ) As ExecuteSelectionResult
+
+        If target Is Nothing Then Throw New ArgumentNullException("target")
+        If act Is Nothing Then Return New ExecuteSelectionResult With {.HasNoAction = True}
+
+        If target.Count = 1 Then
+            Dim a As New TextActionsParameters
+            a.Text = getText(target)
+            act(a)
+            If a.IsCanceled Then Return New ExecuteSelectionResult With {.IsCanceld = True}
+            If setText IsNot Nothing AndAlso a.IsSkipped = False Then
+                setText(target, a.Text)
+            End If
+        Else
+            If target.Rows.Count * target.Columns.Count <> target.Count Then
+
+                Dim c = 1
+                For Each item As Range In target
+                    Dim a As New TextActionsParameters With {
+                        .IsBox = False,
+                        .RowIndex = 1, .ColumnIndex = c
+                    }
+                    a.Text = getText(item)
+                    act(a)
+                    If a.IsCanceled Then Return New ExecuteSelectionResult With {.IsCanceld = True}
+                    If setText IsNot Nothing AndAlso a.IsSkipped = False Then
+                        setText(item, a.Text)
+                    End If
+                    c += 1
+                Next item
+            Else
+                For r = 1 To target.Rows.Count
+                    For c = 1 To target.Columns.Count
+                        Dim item As Range = target.Cells(r, c)
+
+                        Dim a As New TextActionsParameters With {
+                            .IsBox = True,
+                            .RowIndex = r, .ColumnIndex = c}
+                        a.Text = getText(item)
+                        act(a)
+                        If a.IsCanceled Then Return New ExecuteSelectionResult With {.IsCanceld = True}
+                        If setText IsNot Nothing AndAlso a.IsSkipped = False Then
+                            setText(item, a.Text)
+                        End If
+
+                    Next c
+                Next r
+            End If
+        End If
+
+        Return New ExecuteSelectionResult
+    End Function
+
+End Class
+
+
+Friend Class ExecuteSelectionResult
+    Public Property IsCanceld As Boolean
+
+    Public Property HasNoAction As Boolean
+End Class
---
+++ SakuraMacaron.OfficeAddIn.Excel/SakuraMacaron.OfficeAddIn.Excel/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("SakuraMacaron.OfficeAddIn.Excel")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("SakuraMacaron.OfficeAddIn.Excel")> 
+<Assembly: AssemblyCopyright("Copyright ©  2016")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("3af8a93b-7826-48e7-9ee5-e2b7eb0162c8")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
